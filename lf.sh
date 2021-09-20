3db6f01ed920de589a35adccca62470f049c30da
nithin-dot <nithinkirthick@gmail.com>
2021-09-20 20:44:54 +0530

    code added

--- lf.sh
+++ lf.sh
@@ -1,38 +1,129 @@
 
 #!/bin/bash
+# set -e
 exec 2>/dev/null #it prevent from the error
 IFS=$'\n' # To add new line in every list
-
+op=0
 #list directory
 check_dir(){
+if [[ $op -eq 0 ]]
+then
 d=$(ls -rtd */ ) 
 if [[ ! -z $d ]];
 then    
-    echo -e "\n"
   for i in $d 
   do
-     echo -e "  \e[0;91m\e[1m🗀 $i"| sed 's/.$//'   
+     echo -e "  \e[0;91m\e[1m $i"| sed 's/.$//'   
   done
-    echo -e " -$(echo "$d" | wc -l )" "Directory-"
+    echo -e "  -$(echo "$d" | wc -l )" "Directory-\e[0m"
+   
 fi
-
-}
-
+fi
 tf=$(ls)
-if [[ $tf -eq 0 ]] #if the directory is empty it shows
+if [[ $tf = 0 ]] #if the directory is empty it shows
 then 
       echo "This dirctory is Empty,"
 fi
 
+}
+
+
+
+
 # print the value from every funtion
-check_null(){ 
+check_null(){  
 if [[ $1 -ne 0 ]]
 then
       echo -e " -$1 $2-\e[0m"
 fi
 }
+confirmnxt() {
+echo -n "DO you want to BackUp files :"
+read REPLY
+case $REPLY in
+    [Yy]) zipped ;; # you can change what you do here for instance
+    [Nn]) break ;;        # exit case statement gracefully
+    # 1. Repeat the question
+    *) confirmnxt ;;
+esac
+}
 
+confirm(){
+echo "It manage files By transfering Files to respected folder..."
+echo -n "DO you want (C)opy/(M)ove/N(o) the files :"
+read REPLY
+case $REPLY in
+    [Cc]) 
+    cop=1 
+    echo "Files are copying............................" 
+    ;; # you can change what you do here for instance
+    [Mm])
+    mov=1
+    echo "Files are moving.............................";;
+    [Nn]) break ;;        # exit case statement gracefully
+    # 1. Repeat the question
+    *) confirm ;;
+esac
+}
+
+zipped(){
+  
+   varbac=backup_$(date +%m_%d_%Y_%I_%M)
+   $(mkdir backup_$(date +%m_%d_%Y_%I_%M))
+   cD=$(pwd)
+   d=$(ls -rtd */ | sed 's/.$//' ) 
+   if [[ ! -z $d ]];
+   then    
+     for i in $d 
+     do
+      #  echo $i
+      #  echo "$cD/$i"
+      #  echo "$cD/$varbac"
+      #  echo $varbac
+      # echo $varbac
+        if [[ "$i" != "$varbac" ]];
+         then 
+         
+         $(sudo cp -r "$cD/$i" "$cD/$varbac")
+         fi
+     done
+     $(zip -r $varbac.zip $varbac)
+     $(sudo rm -rf $varbac)
+   fi
+    echo "Backup Stored as $varbac.zip"
+}
+filemanage(){
+# echo -e "\e[0m\e[1m"
+# confirm() {
+# echo -n "DO you want to (C)opy/(M)ove :"
+# read REPLY
+# case $REPLY in
+#     [Cc]) echo 'yup c' ;; # you can change what you do here for instance
+#     [Mm]) break ;;        # exit case statement gracefully
+#     # 1. Repeat the question
+#     *) confirm ;;
+# esac
+# }
+# confirm
+   # echo $cop
+   if [ $cop -eq 1 ]
+   then
+   cD=$(pwd)
+   $(mkdir $2)
+   $(sudo cp $cD/$1 $cD/$2)
+   echo -e "\e[1m$i\e[0m Copid to\e[1m $2\e[0m" 
+   fi
+
+   if [ $mov -eq 1 ]
+   then
+   cD=$(pwd)
+   $(mkdir $2)
+   $(sudo mv $cD/$1 $cD/$2)
+   echo -e "\e[1m$i\e[0m Moved to \e[1m$2\e[0m"
+   fi
+}
 #audio_files
+
 check_audio(){
 CAL_AUDIO=0 #variable for assignment
 #exetensions to list
@@ -42,8 +133,15 @@ CAL_AUDIO=0 #variable for assignment
  do
  if [[ ! -z $i ]] #check whether the data is empty
  then
+   if [[ $op -eq 0 ]]
+    then
      CAL_AUDIO=$(($CAL_AUDIO+$(echo "$i" | wc -l)))  # print value
-      echo -e "  \e[3\e[94m𝄞 $i" #giving color
+      echo -e "  \e[3\e[94m $i" #giving color
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i audio_files
+     fi
 fi
 done
 # if [[ $CAL_AUDIO -eq 0 ]] #exe if the fuction don't have output
@@ -63,8 +161,15 @@ CAL_ZIP=0
  do
  if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
      CAL_ZIP=$(($CAL_ZIP+$(echo "$i" | wc -l)))
-      echo -e "  \e[93m\e[1m🤐$i" 
+     echo -e "  \e[93m\e[1m$i"
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i compressed_files
+     fi
 fi
 done
 
@@ -82,11 +187,18 @@ CAL_IMAGE=0
  disc_image=("$(ls *.vcd)" "$(ls *.bin)" "$(ls *.dmg)" "$(ls *.iso)" "$(ls *.toast)")
  for i in ${disc_image[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
      CAL_IMAGE=$(($CAL_IMAGE+$(echo "$i" | wc -l)))
-      echo -e "  \e[1m📄$i"
+      echo -e "  \e[1m$i"
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i bin_files
       
+     fi
 fi
 done
 # if [[ $CAL_IMAGE -eq 0 ]]
@@ -102,10 +214,17 @@ CAL_DATA=0
  database_file=("$(ls *.csv)" "$(ls *.dat)" "$(ls *.db)"  "$(ls *.dbf)"  "$(ls *.log)"  "$(ls *.mdb)"  "$(ls *.sav)"  "$(ls *.sql)"  "$(ls *.xml)")
   for i in ${database_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
      CAL_DATA=$(($CAL_DATA+$(echo "$i" | wc -l)))
-      echo -e "  🗄️\e[90m\e[1m$i" 
+      echo -e "  \e[90m\e[1m$i" 
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i database_files
+     fi
 fi
 done
 
@@ -123,10 +242,17 @@ CAL_EFILE=0
 mail_file=("$(ls *.email)"  "$(ls *.eml)"  "$(ls *.emlx)"  "$(ls *.msg)"  "$(ls *.oft)"  "$(ls *.ost)"  "$(ls *.pst)"  "$(ls *.vcf)")
  for i in ${mail_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
      CAL_EFILE=$(($CAL_EFILE+$(echo "$i" | wc -l)))
-     echo -e "  📮\e[91m\e[1m$i"  
+     echo -e "  \e[91m\e[1m$i"
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i E-mail_files  
+     fi
 fi
 done
 
@@ -145,10 +271,17 @@ CAL_EXEFILE=0
 executable_file=("$(ls *.apk)"  "$(ls *.bat)"  "$(ls *.cgi)"  "$(ls *.pl)"  "$(ls *.exe)"  "$(ls *.gadget)"  "$(ls *.msi)"  "$(ls *.py)"  "$(ls *.wsf)" "$(ls *.sh)")
  for i in ${executable_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
      CAL_EXEFILE=$(($CAL_EXEFILE+$(echo "$i" | wc -l)))
-     echo -e "  ⚙️\e[90m\e[1m $i"
+     echo -e "  \e[90m\e[1m $i"
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i executable_files
+     fi
 fi
 done
 
@@ -167,10 +300,17 @@ CAL_FFILE=0
 Font_file=("$(ls *.fnt)"  "$(ls *.fon)"  "$(ls *.otf)"  "$(ls *.ttf)")
  for i in ${Font_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_FFILE=$(($CAL_FFILE+$(echo "$i" | wc -l)))
-    echo -e "  \e[35m\e[1m🅕 $i" 
+    echo -e "  \e[35m\e[1m $i"
+    fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i Font_files
+     fi
 fi
 done
  
@@ -190,10 +330,17 @@ CAL_IMAFILE=0
  images_format=("$(ls *.ai)" "$(ls *.bmp)" "$(ls *.gif)" "$(ls *.ico)" "$(ls *.jpeg)" "$(ls *.jpg)" "$(ls *.png)" "$(ls *.ps)" "$(ls *.psd)" "$(ls *.svg)" "$(ls *.tif)")
  for i in ${images_format[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_IMAFILE=$(($CAL_IMAFILE+$(echo "$i" | wc -l)))
-    echo -e "  🖼️\e[92m\e[1m $i"   
+    echo -e "  \e[92m\e[1m $i"
+     fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i image_files   
+     fi
 fi
 done
 
@@ -212,10 +359,17 @@ CAL_INTERFILE=0
  internet_based_file=("$(ls *.asp)"  "$(ls *.cer)"  "$(ls *.cfm)"  "$(ls *.cgi)"  "$(ls *.pl)"  "$(ls *.css)"  "$(ls *.htm)"  "$(ls *.js)"  "$(ls *.jsp)"  "$(ls *.part)"  "$(ls *.php)"  "$(ls *.py)"  "$(ls *.rss)"  "$(ls *.xhtml)")
  for i in ${internet_based_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_INTERFILE=$(($CAL_INTERFILE+$(echo "$i" | wc -l)))
-    echo -e "  \e[90m\e[1m🕸️ $i"
+    echo -e "  \e[90m\e[1m $i"
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i Internet_files  
+     fi
 fi
 done
 
@@ -234,10 +388,17 @@ CAL_PPTE=0
  presentation_format=("$(ls *.key)"  "$(ls *.odp)"  "$(ls *.pps)"  "$(ls *.ppt)"  "$(ls *.pptx)")
  for i in ${presentation_format[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_PPTE=$(($CAL_PPTE+$(echo "$i" | wc -l)))
-    echo -e "  \e[34m\e[1m🕮 $i" 
+    echo -e "  \e[34m\e[1m $i"
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i presentation_files 
+     fi
 fi
 done
 
@@ -256,10 +417,17 @@ CAL_EXCEL=0
  Spreadsheet_file=("$(ls *.ods)"  "$(ls *.xls)"  "$(ls *.xlsm)"  "$(ls *.xlsx)")
  for i in ${Spreadsheet_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_EXCEL=$(($CAL_EXCEL+$(echo "$i" | wc -l)))
-      echo -e "  \e[32m\e[1m❎$i" 
+      echo -e "  \e[32m\e[1m$i" 
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i Spreadsheet_files
+     fi
 fi
 done
 
@@ -277,10 +445,17 @@ CAL_SYSFILE=0
  system_file=("$(ls *.bak)"  "$(ls *.cab)"  "$(ls *.cfg)"  "$(ls *.cpl)"  "$(ls *.cur)"  "$(ls *.dll)"  "$(ls *.dmp)"  "$(ls *.drv)"  "$(ls *.icns)"  "$(ls *.ico)"  "$(ls *.ini)"  "$(ls *.lnk)"  "$(ls *.sys)"  "$(ls *.tmp)")
  for i in ${system_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
    CAL_SYSFILE=$(($CAL_SYSFILE+$(echo "$i" | wc -l)))
-    echo -e "  💻\e[1m$i" 
+    echo -e "  \e[1m$i"
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i system_files 
+     fi
 fi
 done
 
@@ -299,10 +474,17 @@ CAL_VIDEO=0
  Video_file_format=("$(ls *.3g2)"  "$(ls *.3gp)"  "$(ls *.avi)"  "$(ls *.flv)"  "$(ls *.h264)"  "$(ls *.m4v)"  "$(ls *.mkv)"  "$(ls *.mov)"  "$(ls *.mp4)"  "$(ls *.mpg)"  "$(ls *.mpeg)"  "$(ls *.rm)"  "$(ls *.swf)"  "$(ls *.vob)"  "$(ls *.wmv)")
  for i in ${Video_file_format[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_VIDEO=$(($CAL_VIDEO+$(echo "$i" | wc -l)))
-    echo -e "  🎥\e[90m\e[1m$i" 
+    echo -e "  \e[90m\e[1m$i"
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i Video_files 
+     fi
 fi
 done
 
@@ -321,10 +503,18 @@ CAL_WORD=0
  Word_process_file=("$(ls *.doc)"  "$(ls *.docx)"  "$(ls *.odt)"  "$(ls *.pdf)"  "$(ls *.rtf)"  "$(ls *.tex)"  "$(ls *.txt)"  "$(ls *.wpd)")
  for i in ${Word_process_file[@]}
  do
- if [[ ! -z $i ]]
+ if [[ ! -z $i ]] 
  then
+    if [[ $op -eq 0 ]]
+    then
     CAL_WORD=$(($CAL_WORD+$(echo "$i" | wc -l)))
-       echo -e "  🗒️\e[0m\e[1m $i"
+       echo -e "  \e[0m\e[1m$i"
+      fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i txt_files
+       
+     fi
 fi
 done
 
@@ -339,14 +529,21 @@ check_null $CAL_WORD word
 
 #program_files
 check_program(){
- CAL_PROGRAM=0
+ CAL_PROGRAM=0 
   program_file=("$(ls *.h)" "$(ls *.c)" "$(ls *.cpp)" "$(ls *.class)" "$(ls *.java)" "$(ls *.php)" "$(ls *.cs)" "$(ls *.swift)" "$(ls *.vb)" "$(ls *.dart)" "$(ls *.ts)" "$(ls *.tsx)" "$(ls *.sc)"  "$(ls *.scala)" "$(ls *.scm)" "$(ls *.kt)" "$(ls *.kts)" "$(ls *.ktm)" "$(ls *.m)" "$(ls *.mat)" "$(ls *.go)" "$(ls *.rb)" "$(ls *.r)" "$(ls *.rdata)" "$(ls *.rds)" "$(ls *.rda)" "$(ls *.py)")
   for i in ${program_file[@]}
   do
   if [[ ! -z $i ]]
   then
+   if [[ $op -eq 0 ]]
+    then
     CAL_PROGRAM=$(($CAL_PROGRAM+$(echo "$i" | wc -l)))
-      echo -e "  👨‍💻\e[93m\e[1m$i" 
+    echo -e "  \e[93m\e[1m$i"
+   fi
+    if [[ $op -eq 1 ]]
+    then
+     filemanage $i program_files
+    fi
 fi
 done
 
@@ -418,8 +615,7 @@ _EOF_
 # if [ "$1" == "-zip" ]; then
 # 	check_zip
 # fi
-if [[ ! $# -eq 1 ]]
-then
+list(){
 check_dir
 check_zip
 check_audio
@@ -435,8 +631,21 @@ check_sys
 check_video
 check_program
 check_word
+}
+ask(){
+   op=1
+   confirm
+   list
+   confirmnxt
+}
+
+if [[ ! $# -eq 1 ]]
+then
+   list
+   ask
 #     echo "use lf -help to know how to use this command :)"
 fi
+
 
 
 while [ ! $# -eq 0 ]
